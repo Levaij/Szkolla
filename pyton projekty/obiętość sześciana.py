3c853c416801d6122f10467710e2bad1d49b3cfd
Levaij <102971137+Levaij@users.noreply.github.com>
2022-10-01 01:09:03 +0200

    Add files via upload

---
+++ pyton projekty/obiętość sześciana.py
@@ -0,0 +1,32 @@
+#Nikola Beslic praca na 6!
+
+print("Podaj bok a" + "\n")
+
+a = float(input())
+
+print("Podaj bok b" + "\n")
+
+b = float(input())
+
+print("Podaj bok c" + "\n")
+
+c = float(input())
+
+print("Bok a wynosi" , a)
+print("Bok a wynosi" , b)
+print("Bok a wynosi" , c)
+
+
+print("\n" + "Opcje:" + "\n" + "\n" "1: Obiętość")
+
+X = float(input())
+
+if X == 1:
+    p = float(a * b)
+    o = float(p * c)
+    print("obiętość tego graniastosłupa wyosi" , o)
+else:
+    print("Nie ma takiej opcji")
+    
+
+
