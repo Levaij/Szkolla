e3a38d11aee5ecc7840e436d99955b015a08593d
Levaij <102971137+Levaij@users.noreply.github.com>
2022-10-11 18:02:59 +0200

    Add files via upload

---
+++ szkola praca 8/script.js
@@ -0,0 +1,11 @@
+var przycisk = document.getElementById("skrypt")
+var napis = document.getElementById("ksztalt").value
+var napisp = document.getElementById("zamowienie")
+
+function zmiana() {
+    if(napis == "1"){
+        alert("1")
+    }else if(napis == "2"){
+        alert("2")
+    }
+}
