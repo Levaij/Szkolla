e9e740173a95f7b9b7e2d9e82be021dd3afbe70c
Levaij <102971137+Levaij@users.noreply.github.com>
2023-01-03 16:26:32 +0100

    Add files via upload

--- giga matma projekt 1/script.js
+++ giga matma projekt 1/script.js
@@ -1,3 +1,52 @@
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+function quadratic(a, b) {
+    return function(x) {
+      return a * x * x + b * x ;
+    }
+}
+    const chartArea = {
+    x: 50,
+    y: 10,
+    width: 200,
+    height: 300
+};
+    const xMin = -10;
+    const xMax = 10;
+    const yMin = -10;
+    const yMax = 10;
+
+function xToPixel(x) {
+    return chartArea.x + (x - xMin) / (xMax - xMin) * chartArea.width;
+}
+  
+  function yToPixel(y) {
+    return chartArea.y + chartArea.height - (y - yMin) / (yMax - yMin) * chartArea.height;
+}
+
+
+    ctx.beginPath();
+    ctx.moveTo(xToPixel(xMin), yToPixel(0));
+    ctx.lineTo(xToPixel(xMax), yToPixel(0));
+    ctx.moveTo(xToPixel(0), yToPixel(yMin));
+    ctx.lineTo(xToPixel(0), yToPixel(yMax));
+    ctx.stroke();
+
+function plotQuadratic(a, b, c) {
+
+    const quadFunc = quadratic(a, b, c);
+  
+    ctx.beginPath();
+    ctx.moveTo(xToPixel(xMin), yToPixel(quadFunc(xMin)));
+  
+    for (let x = xMin + 0.1; x < xMax; x += 0.1) {
+        ctx.lineTo(xToPixel(x), yToPixel(quadFunc(x)));
+    }
+  
+    ctx.stroke();
+
+};  // plots the curve y = x^2
+
 function kwadratowa(){
 
     var a = prompt("podaj a (x²)");
@@ -6,22 +55,48 @@ function kwadratowa(){
         kwadratowa();
     }
     var b = prompt("podaj b (x)");
+
+
+    if (c!==0) {
+        var funkcja = `Funkcja Kwadratowa: f(x)=${a}x² + ${b}x`;
+    }
+    else{
+        var funkcja = `Funkcja Kwadratowa: f(x)=${a}x² + ${b}x + ${c}`;
+    }
+    
+
     var c = prompt("podaj c");
-    
-    
     var delta = (b * b - (4 * a * c));
+
+    var p = (-b / (a * 2))
+    var q = (-delta / (a *4))
+
+    var kanoniczna = ("Postać Kanoniczna: f(x) = " + a + " + (x - " + p + ") + " + q)
+    console.log(funkcja)
+    console.log(kanoniczna)
     
     if (delta > 0){
     
         var x1 = ((-b - Math.sqrt(delta)) / (a * 2));
     
         var x2 = ((-b + Math.sqrt(delta)) / (a * 2));
-        document.getElementById("kwadratowa").innerHTML= "miejsce zerowe x1 = " + x1 + "<br> miejsce zerowe x2 = " + x2;
+
+        var iloczynowa = ("Postać Iloczynowa: f(x) + " + a + "(x - " + x1 + ")(x - " + x2 + ")")
+        document.getElementById("kwadratowa").innerHTML= (funkcja + "<br> Miejsce Zerowe x1 = " + x1 + "<br> Miejsce Zerowe x2 = " + x2 + "<br>" + kanoniczna + "<br>" + iloczynowa);
+
+        
+        console.log(iloczynowa)
+        canvas.style.display="block"
+        plotQuadratic(a,b)
     
     }
     else if (delta === 0){
         var x0 = (-b / (a * 2));
-        document.getElementById("kwadratowa").innerHTML= "miejsce zerowe x0 = " + x0 + "</br>";
+
+        var iloczynowa = ("Postać Iloczynowa: f(x) + " + a + "(x - " + x0 + ")²")
+        document.getElementById("kwadratowa").innerHTML= (funkcja + "<br> Miejsce Zerowe x0 = " + x0 + "</br>" + kanoniczna + "<br>" + iloczynowa);
+
+        console.log(iloczynowa)
     }
     
     else if(delta <0){
@@ -79,4 +154,3 @@ function Paliwo(){
     document.getElementById("Paliwo").innerHTML=((x).toFixed(2) + "zł");
 
 } 
-
